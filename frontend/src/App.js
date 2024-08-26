5f063b4d7fe3c61b92d572338c83dd4a648be360
hooniverse <sung010326@gmail.com>
2024-08-26 17:59:55 +0900

    app.js 수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import Header from './component/Header';
-import UserInfo from './component/UserInfo';
-import Menu from './component/Menu';
-import Map from './component/Map';
 
 const App = () => {
   return (
     <div className="app-container">
-      <Header />
-      <UserInfo />
-      <Menu />
-      <Map />
+      
     </div>
   );
 };
